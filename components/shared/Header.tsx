9637880095438cac7e13b87a1fd2f6e3d5ea2e14
PerpetuumG <perpetuumg@gmail.com>
2024-01-15 16:21:27 +0200

    Add Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={'w-full border-b'}>
+      <div className={'wrapper flex items-center justify-between'}>
+        <Link href={'/'} className={'w-36'}>
+          <Image src={'/assets/images/logo.svg'} alt={'Evently logo'} width={128} height={38} />
+        </Link>
+
+        <div className={'flex w-32 justify-end gap-3'}></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
