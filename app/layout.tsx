e6bb3217f34e6265093a0c0ba150705531cff2cb
PerpetuumG <perpetuumg@gmail.com>
2024-01-16 16:10:06 +0200

    add Clerk, Nav, MobileNav, Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
+import React from 'react';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,8 +21,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
